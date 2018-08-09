ba26021b1b9e4bbd76a5cd593d8cecd634aa98af
Jordan Yu <jordanyu44@gmail.com>
2018-08-09 14:36:34 -0700

    Build backend auth structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,5 +28,36 @@ class User < ApplicationRecord
            source: :board
 
   attr_reader :password
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+  private
+
+  def generate_session_token
+    SecureRandom.urlsafe_base64
+  end
 
 end
