7aef0045fec0da71a5365221c72fa21b2ac26f1c
Jordan Yu <jordanyu44@gmail.com>
2018-08-09 14:58:52 -0700

    Set up backend routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:show, :create]
+    resource :session, only: [:create, :destroy]
+    resources :boards, only: [:show, :index, :create, :update, :destroy]
+    resources :lists, only: [:show, :index, :create, :update, :destroy]
+    resources :cards, only: [:show, :index, :create, :update, :destroy]
+    resources :labels, only: [:index, :create, :update, :destroy]
+  end
+
+  root "static_pages#root"
+
 end
