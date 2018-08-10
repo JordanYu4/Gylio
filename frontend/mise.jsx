a1aed226b95eca8e516caf63503db690b121bafe
Jordan Yu <jordanyu44@gmail.com>
2018-08-10 10:49:03 -0700

    Build root and app components; update entry file

--- frontend/mise.jsx
+++ frontend/mise.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {signup, login, logout} from './util/session_api_util'
-
-window.signup = signup;
-window.login = login;
-window.logout = logout;
+import Root from './components/root';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  // TESTING START
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // TESTING END
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to getting stuff done</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
