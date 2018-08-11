6a8497ae4435d2b2ef13b2383d353c4d06c00d31
Jordan Yu <jordanyu44@gmail.com>
2018-08-11 14:05:42 -0700

    Rename app2 to App

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import SessionForm  from './session_form/session_form';
-import App from './app2';
+import App from './App';
 
 const Root = ({ store }) => (
   <Provider store={ store }>
