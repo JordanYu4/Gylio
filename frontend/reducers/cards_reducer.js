f465c0fe968ed8153059f5edb5ac5d8deea5720e
Jordan Yu <jordanyu44@gmail.com>
2018-08-15 17:45:10 -0700

    Build cards reducer

---
+++ frontend/reducers/cards_reducer.js
@@ -0,0 +1,21 @@
+import merge from 'merge/lodash';
+
+import {
+  RECEIVE_CARDS,
+  RECEIVE_CARD
+} from '../../actions/card_actions';
+
+const CardsReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_CARDS:
+      return action.cards;
+    case RECEIVE_CARD:
+      return merge({}, state, {[action.card.id]: action.card});
+    default:
+      return state;
+  }
+};
+
+export const CardsReducer;
