973396bae626aedf27c46c2272f7b17691479865
Jordan Yu <jordanyu44@gmail.com>
2018-08-20 11:17:10 -0700

    Revert webpack config to ignore plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,4 @@
 var path = require("path");
-// var webpack = require("webpack");
-
-// var plugins = [];
-// var devPlugins = [];
-//
-// var prodPlugins = [
-//   new webpack.DefinePlugin({
-//     'process.env': {
-//       'NODE_ENV': JSON.stringify('production')
-//     }
-//   }),
-//   new config.optimization.minimize({
-//     compress: {
-//       warnings: true
-//     }
-//   })
-// ];
-//
-// plugins = plugins.concat(
-//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-// );
 
 module.exports = {
   context: __dirname,
@@ -28,7 +7,6 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
-  // plugins: plugins,
   module: {
     rules: [
       {
