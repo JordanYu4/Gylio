b151705ef58764b8174bd158440c386c5a16529e
Jordan Yu <jordanyu44@gmail.com>
2018-09-21 21:45:42 -0700

    Change board form container from jsx to js

--- frontend/util/route_util.js
+++ frontend/util/route_util.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { withRouter, Redirect, Route } from 'react-router-dom';
+import { 
+  withRouter, 
+  Redirect, 
+  Route 
+} from 'react-router-dom';
 
 const Auth = ({component: Component, path, loggedIn, exact}) => (
   <Route path={path} exact={exact} render={(props) => (
@@ -22,12 +26,9 @@ const Protected = ({component: Component, path, loggedIn, exact}) => (
     )}/>
 );
 
-
-
 const mapStateToProps = state => {
   return {loggedIn: Boolean(state.session.id)};
 };
 
 export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
-
 export const ProtectedRoute = withRouter(connect(mapStateToProps, null)(Protected));
