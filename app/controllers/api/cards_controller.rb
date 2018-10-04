db79acbeabfb3cde54ac607edac083966c8b1237
Jordan Yu <jordanyu44@gmail.com>
2018-10-04 14:50:15 -0700

    Build out cards controller methods

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -3,30 +3,46 @@ class Api::CardsController < ApplicationController
 
   def show
     @card = Card.find(params[:id])
-    render :json
+    render :show
   end
 
   def index
-    
-    render :json
+    current_list = List.find(params[:list_id])
+    @cards = current_list.cards
+    render :index
   end
 
   def create
-    @card = Card.new(card_params)
-    unless @card.save
+    @card = Card.new(create_card_params)
+    if @card.save
+      render :show
+    else
       render json: @card.errors.full_messages, status: 422
     end
   end
 
   def update
+    @card = Card.find(params[:id])
+    @card.update_attributes(edit_card_params)
+    if @card.save
+      render :show
+    else
+      render json: @card.errors.full_messages, status: 422
+    end
   end
 
   def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
   end
 
   private
 
-  def card_params
+  def create_card_params
+    params.require(:card).permit(:title)
+  end
+  
+  def edit_card_params
     params.require(:card).permit(:title)
   end
 
