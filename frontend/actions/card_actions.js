dcd0d3ddf68953c3b57c7b5f64f6c8b120e2ee5e
Jordan Yu <jordanyu44@gmail.com>
2018-10-10 09:55:16 -0700

    Build out card actions

--- frontend/actions/card_actions.js
+++ frontend/actions/card_actions.js
@@ -2,6 +2,7 @@ import * as CardAPIUtil from '../util/card_api_util';
 
 export const RECEIVE_CARDS = 'RECEIVE_CARDS';
 export const RECEIVE_CARD = 'RECEIVE_CARD';
+export const REMOVE_CARD = 'REMOVE_CARD';
 export const RECEIVE_CARD_ERRORS = 'RECEIVE_CARD_ERRORS';
 
 // export const fetchCards = () => dispatch => (
@@ -27,9 +28,19 @@ export const createCard = card => dispatch => (
 );
 
 export const editCard = card => dispatch => (
-  CardAPIUtil.updateCard(card).then(payload => 
+  CardAPIUtil.updateCard(card).then(payload => ( 
     dispatch(receiveCard(payload))
-  )
+  ), errors => (
+    dispatch(receiveErrors(errors.responseJSON))
+  ))
+);
+
+export const deleteCard = cardId => dispatch => (
+  ListAPIUtil.deleteCard(cardId).then(cardId => (
+    dispatch(removeCard(cardId))
+  ), errors => (
+    dispatch(receiveErrors(errors.responseJSON))
+  ))
 );
 
 export const receiveCards = cards => ({
@@ -37,10 +48,15 @@ export const receiveCards = cards => ({
   cards
 });
 
-export const receiveCard = card => ({
+export const receiveCard = payload => ({
   type: RECEIVE_CARD,
-  card
+  payload
 });
+
+export const removeCard = cardId => ({
+  type: REMOVE_CARD, 
+  cardId
+})
 
 export const receiveErrors = errors => ({
   type: RECEIVE_CARD_ERRORS,
