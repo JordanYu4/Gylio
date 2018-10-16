b99828acd266a2949fdba5cf9d0fb17d5ae32d07
Jordan Yu <jordanyu44@gmail.com>
2018-10-16 14:12:05 -0700

    Add temp card detail placeholder

--- frontend/components/cards/card_detail.jsx
+++ frontend/components/cards/card_detail.jsx
@@ -8,7 +8,8 @@ class CardDetail extends React.Component {
   render() {
     return(
       <div className="card-detail-container modal-form">
-
+        <span>Card detail currently under construction.</span>
+        <span>We appreciate your patience!</span>
       </div>
     );
   }
