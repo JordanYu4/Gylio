f31c1e4bdd6f14824625974ffc9fc72c11e322fe
Jordan Yu <jordanyu44@gmail.com>
2018-10-16 21:25:30 -0700

    Clean up splash footer styling

--- frontend/components/splash/splash_footer_links.jsx
+++ frontend/components/splash/splash_footer_links.jsx
@@ -2,37 +2,37 @@ import React from "react";
 // import { Link } from "react-router-dom";
 
 const FooterLinks = () => (
-  <div className="footer-links-container">
-    <ul className="footer-links">
-      <div className="internal-links">
-        <li className="footer-link">
-          {/* <Link to="/contact">Careers</Link> */}
-          <a 
-            href="https://www.linkedin.com/in/jordan-yu-4400/"
-            target="_blank"
-          >
-            Careers
-          </a>
-        </li>
-        <li className="footer-link">
-          {/* <Link to="/contact">Contact</Link> */}
-          <a
-            href="https://www.linkedin.com/in/jordan-yu-4400/"
-            target="_blank"
-          >
-            Careers
-          </a>
-        </li>
-      </div>
+  <section className="footer-links-container">
+    {/* <section className="footer-links"> */}
+      {/* <section> */}
+        {/* <Link to="/contact">Careers</Link> */}
       <a
-        className="github-footer"
+        href="https://www.linkedin.com/in/jordan-yu-4400/"
+        target="_blank"
+        className="footer-link"
+      >
+        Careers
+      </a>
+      {/* </section> */}
+      {/* <section className="footer-link"> */}
+        {/* <Link to="/contact">Contact</Link> */}
+      <a
+        href="https://www.linkedin.com/in/jordan-yu-4400/"
+        target="_blank"
+        className="footer-link"
+      >
+        Contact Us
+      </a>
+      {/* </section> */}
+      <a
+        className="footer-link"
         href="https://github.com/JordanYu4/Gylio"
         target="_blank"
       >
         <i className="icon-github" />
       </a>
-    </ul>
-  </div>
+    {/* </section> */}
+  </section>
 );
 
 export default FooterLinks;
