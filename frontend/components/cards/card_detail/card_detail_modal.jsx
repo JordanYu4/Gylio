09f8a03da0b875111c668fe5b57ac6999a917dce
Jordan Yu <jordanyu44@gmail.com>
2018-10-31 14:14:39 -0700

    Refactor card detail modal into class

--- frontend/components/cards/card_detail/card_detail_modal.jsx
+++ frontend/components/cards/card_detail/card_detail_modal.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 
 import CardDetailContainer from './card_detail_container';
 
-const CardDetailModal = () => (
-  <div className="modal">
-    <section className="modal-screen js-modal-close"></section>
-    <CardDetailContainer cardId={277}/>
-  </div>
-);
+class CardDetailModal extends React.Component {
+  constructor(props) {
+    super(props);
+  } 
+
+  
+
+  render() {
+
+    return (
+      <div className="modal">
+        <section className="modal-screen js-modal-close"></section>
+        <CardDetailContainer cardId={277}/>
+      </div>
+    );
+  }
+};
 
 export default CardDetailModal;
