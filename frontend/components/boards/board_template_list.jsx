6dd85bcc3e2fa377f064ddd7d5ea4709cdec5f50
Jordan Yu <jordanyu44@gmail.com>
2018-11-15 22:40:38 -0800

    Set up board template index component

--- frontend/components/boards/board_template_list.jsx
+++ frontend/components/boards/board_template_list.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import BoardIndexItem from './board_index_item';
 
 const BoardTemplateList = () => {
+  const boards = [];
+  /* Clicking on a board template index item 
+     (similar to a BoardIndexItem) should create
+     a new personal board with pre-made background, 
+     lists, and some cards */ 
+  const indexedTemplates = boards.map(board => (
+    <BoardIndexItem
+      board={board}
+      key={board.id}
+    />
+  ));
 
   return (
-    <div></div>
+    <ul className="board-index-list">
+      { indexedTemplates }
+    </ul>
   );
 };
 
